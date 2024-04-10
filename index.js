4fe5445104eb516029bcce0df3ee896b62470d3f
Brian Kipkorir <rotichbrian891@gmail.com>
2024-04-10 16:00:19 +0300

    Add event listener to weatherForm to prevent default form submission

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const cityInput = document.querySelector(".cityInput");
 const weatherattributes = document.querySelector(".weatherattributes")
 
 const apiKey = "afc368f03905f3e4ae3918426c6cbfee";
-       
+ 
+weatherForm.addEventListener("submit", async event => {
+      
+    event.preventDefault();
+})

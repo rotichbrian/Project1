74d45e9e84e32dfceb3ebdc4454a48c75fd3a0cc
Brian Kipkorir <rotichbrian891@gmail.com>
2024-04-10 15:56:38 +0300

    initial setup for weather form, city input, and weather attributes; include API key for weather data fetching.

---
+++ index.js
@@ -0,0 +1,6 @@
+const weatherForm = document.querySelector(".weatherform");
+const cityInput = document.querySelector(".cityInput");
+const weatherattributes = document.querySelector(".weatherattributes")
+
+const apiKey = "afc368f03905f3e4ae3918426c6cbfee";
+       
